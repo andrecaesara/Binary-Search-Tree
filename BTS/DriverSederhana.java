b8bcc0abfbc6fdf43aeba6730f442612a915a0f2
andrecaesara <andrekovling@gmail.com>
2017-12-13 21:07:24 +0700

    Add files via upload

---
+++ BTS/DriverSederhana.java
@@ -0,0 +1,12 @@
+package BTS;
+
+public class DriverSederhana {
+    public static void main(String[] args) {
+        BinarySearchTree bt =new BinarySearchTree();
+        bt.insert(1);
+        bt.insert(2);
+        bt.insert(3);
+        System.out.println(bt.contains(2));
+        
+    }
+}
